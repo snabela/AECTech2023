2bac5eab106ed836ca56ba1942ad5bb62607b885
Stijn Jansen <sjansen@viktor.ai>
2023-11-04 21:07:44 -0400

    add optimization

--- viktor/app.py
+++ viktor/app.py
@@ -1,9 +1,13 @@
 from pathlib import Path
+import plotly.express as px
 
 from viktor import ViktorController, File
 from viktor.geometry import GeoPoint
-from viktor.parametrization import ViktorParametrization, Page, GeoPointField, Tab, OptionField
-from viktor.views import MapView, MapResult, MapPoint, GeometryView, GeometryResult, WebView, WebResult
+from viktor.parametrization import ViktorParametrization, Page, GeoPointField, Tab, OptionField, NumberField
+from viktor.views import MapView, MapResult, MapPoint, GeometryView, GeometryResult, WebView, WebResult, PlotlyView, \
+    PlotlyResult
+
+from optimization.run_optimization import run_optimization
 
 
 class Parametrization(ViktorParametrization):
@@ -19,7 +23,23 @@ class Parametrization(ViktorParametrization):
     structural.wind = Tab('Wind')
     structural.wind.code = OptionField('Code', options=['ASCE41', 'ACI'])
 
-    optimization = Page('Optimization')
+    optimization = Page('Optimization', views='get_optimization')
+    optimization.min_base_radius = NumberField('Min Base Radius', default=60)
+    optimization.max_base_radius = NumberField('Max Base Radius', default=200)
+    optimization.step_base_radius = NumberField('Step Base Radius', default=10)
+
+    optimization.min_peak_radius = NumberField('Min Peak Radius', default=60)
+    optimization.max_peak_radius = NumberField('Max Peak Radius', default=100)
+    optimization.step_peak_radius = NumberField('Step Peak Radius', default=10)
+
+    optimization.min_no_floors = NumberField('Min No Floors', default=10)
+    optimization.max_no_floors = NumberField('Max No Floors', default=50)
+    optimization.step_no_floors = NumberField('Step No Floors', default=5)
+
+    optimization.min_floor_to_floor = NumberField('Min Floor to Floor', default=8)
+    optimization.max_floor_to_floor = NumberField('Max Floor to Floor', default=15)
+    optimization.step_floor_to_floor = NumberField('Step Floor to Floor', default=1)
+
     # TODO add necessary input parameters
 
 
@@ -49,3 +69,13 @@ class Controller(ViktorController):
     def get_web_view(self, params, **kwargs):
         html_path = Path(__file__).parent / 'detailedmap_3d.html'
         return WebResult.from_path(html_path)
+
+    @PlotlyView("Result", duration_guess=1)
+    def get_optimization(self, params, **kwargs):
+        dimensions = ['Base Radius', 'Peak Radius', 'No Floors', 'Floor to Floor', 'Cost', 'Embodied Carbon']
+        df = run_optimization(params, dimensions)
+        fig = px.parallel_coordinates(df, color="Embodied Carbon",
+                                      dimensions=dimensions,
+                                      color_continuous_scale=px.colors.diverging.Tealrose,
+                                      color_continuous_midpoint=2)
+        return PlotlyResult(fig.to_json())
